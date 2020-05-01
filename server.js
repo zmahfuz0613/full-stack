7b70bfa99a5f570e8c8e81287bd9c26f245f0ad3
Bruno Galvao <brunopgalvao@gmail.com>
2020-05-01 16:32:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const app = require('./app.js')
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const routes = require('./routes');
 const db = require('./db')
-
 const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+app.use('/api', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
